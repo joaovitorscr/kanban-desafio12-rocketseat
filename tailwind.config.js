a73f55b3b3c338a067b70a7f3a6e7dd8db5fc7ed
joaovitorscr <contato.joaovitorscr@gmail.com>
2023-10-13 13:02:44 -0300

    ADD: Layout, styles, custom color, utility classes.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple: '#7c3aed',
+      },
+      boxShadow: {
+        cardShadow: '0 4px 16px 0px #eae2fd',
+      },
+    },
   },
   plugins: [],
 }
